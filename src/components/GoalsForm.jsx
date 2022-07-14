f7a2055a6378105b7d5c4de2046ecb0886f22f5d
Bradley-Bird <Bradleybird2@icloud.com>
2022-07-14 09:57:10 -0700

    fixed warnings with mui syntax on GoalForm

--- src/components/GoalsForm.jsx
+++ src/components/GoalsForm.jsx
@@ -25,85 +25,75 @@ export default function GoalsForm() {
             <ListItemAvatar>
               <FeedIcon />
             </ListItemAvatar>
-            <ListItemText
-              primary="How many applications do you plan to submit?"
-              secondary={
-                <>
-                  <TextField
-                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                    /* TODO: add accessibility arrows, labels, etc */
-                  />
-                </>
-              }
-            />
+            <Box>
+              <ListItemText primary="How many applications do you plan to submit?" />
+              <>
+                <TextField
+                  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                  /* TODO: add accessibility arrows, labels, etc */
+                />
+              </>
+            </Box>
           </ListItem>
           <Divider variant="inset" component="li" />
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <FeedIcon />
             </ListItemAvatar>
-            <ListItemText
-              primary="How many networking chats do you plan to have?"
-              secondary={
-                <>
-                  <TextField
-                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                    /* TODO: add accessibility arrows, labels, etc */
-                  />
-                </>
-              }
-            />
+            <Box>
+              <ListItemText primary="How many networking chats do you plan to have?" />
+              <>
+                <TextField
+                  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                  /* TODO: add accessibility arrows, labels, etc */
+                />
+              </>
+            </Box>
           </ListItem>
           <Divider variant="inset" component="li" />
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <FeedIcon />
             </ListItemAvatar>
-            <ListItemText
-              primary="How many meetups do you plan to attend?"
-              secondary={
-                <>
-                  <TextField
-                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                    /* TODO: add accessibility arrows, labels, etc */
-                  />
-                </>
-              }
-            />
+            <Box>
+              <ListItemText primary="How many meetups do you plan to attend?" />
+              <>
+                <TextField
+                  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                  /* TODO: add accessibility arrows, labels, etc */
+                />
+              </>
+            </Box>
           </ListItem>
           <Divider variant="inset" component="li" />
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <FeedIcon />
             </ListItemAvatar>
-            <ListItemText
-              primary="How many new LinkedIn connections do you plan to make?"
-              secondary={
-                <>
-                  <TextField
-                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                    /* TODO: add accessibility arrows, labels, etc */
-                  />
-                </>
-              }
-            />
+            <Box>
+              <ListItemText primary="How many new LinkedIn connections do you plan to make?" />
+              <>
+                <TextField
+                  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                  /* TODO: add accessibility arrows, labels, etc */
+                />
+              </>
+            </Box>
           </ListItem>
           <Divider variant="inset" component="li" />
           <ListItem alignItems="flex-start">
             <ListItemAvatar>
               <FeedIcon />
             </ListItemAvatar>
-            <ListItemText
-              primary="How many hours do you plan to work on code?"
-              secondary={
-                <>
-                  <TextField
-                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                    /* TODO: add accessibility arrows, labels, etc */
-                  />
-                </>
-              }
-            />
+            <Box>
+              <ListItemText primary="How many hours do you plan to work on code?" />
+              <>
+                <TextField
+                  inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                  /* TODO: add accessibility arrows, labels, etc */
+                />
+              </>
+            </Box>
           </ListItem>
           <Divider variant="inset" component="li" />
         </List>
