4137fc4516e9b48553d420b41a741223159ee807
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-15 12:31:03 -0700

    added updateJob function to services

--- src/services/jobs.js
+++ src/services/jobs.js
@@ -13,3 +13,20 @@ export async function fetchJobs() {
 
   return await jobs.json();
 }
+
+export async function updateJob({ id, status }, index) {
+  const url = `${url}/api/v1/trackers/${id}`;
+  const requestInfo = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    mode: 'cors',
+    body: JSON.stringify({ index, status }),
+  };
+  const updatedData = await fetch(url, requestInfo);
+  if (!updatedData.ok) {
+    throw new Error(`Error fetching job with id: ${id}`);
+  }
+  const job = await updatedData.json();
+  return job;
+}
