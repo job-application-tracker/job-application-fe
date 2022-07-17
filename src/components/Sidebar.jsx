ef4f212c3f44ada2c516934f21ae2540b17e7779
aidanliddiard <aidanliddiard@gmail.com>
2022-07-17 11:55:07 -0700

    added buttons to on sidebar to increase and decrease achievements

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Container, Typography } from '@mui/material';
+import { Button, ButtonGroup, Container, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useUserContext } from '../context/userContext';
@@ -6,8 +6,8 @@ import { getAchievementsByWeek } from '../services/achievements';
 import DisplayPercentComplete from './DisplayPercentComplete';
 
 function Sidebar() {
-  const [achieved, setAchieved] = useState({});
   const { currentUser } = useUserContext();
+  const [achieved, setAchieved] = useState({});
 
   useEffect(() => {
     try {
@@ -32,6 +32,15 @@ function Sidebar() {
 
   console.log('achieved', achieved);
 
+  const handleButtonClick = async (achType, value) => {
+    if (value < 0 && achieved[achType] === 0) return;
+    const update = achieved[achType] + value;
+    const newObj = { ...achieved };
+    newObj[achType] = update;
+    // call backend
+    setAchieved(newObj);
+  };
+
   return (
     <Container maxWidth="xs">
       <Typography component="h3">Applications</Typography>
@@ -39,26 +48,46 @@ function Sidebar() {
         complete={achieved.appNum}
         total={currentUser.appGoal}
       />
+      <ButtonGroup variant="contained">
+        <Button onClick={() => handleButtonClick('appNum', -1)}>-</Button>
+        <Button onClick={() => handleButtonClick('appNum', 1)}>+</Button>
+      </ButtonGroup>
       <Typography component="h3">Networking</Typography>
       <DisplayPercentComplete
         complete={achieved.networkNum}
         total={currentUser.networkGoal}
       />
+      <ButtonGroup variant="contained">
+        <Button onClick={() => handleButtonClick('networkNum', -1)}>-</Button>
+        <Button onClick={() => handleButtonClick('networkNum', 1)}>+</Button>
+      </ButtonGroup>
       <Typography component="h3">Meetups</Typography>
       <DisplayPercentComplete
         complete={achieved.meetupNum}
         total={currentUser.meetupGoal}
       />
+      <ButtonGroup variant="contained">
+        <Button onClick={() => handleButtonClick('meetupNum', -1)}>-</Button>
+        <Button onClick={() => handleButtonClick('meetupNum', 1)}>+</Button>
+      </ButtonGroup>
       <Typography component="h3">LinkedIn connections</Typography>
       <DisplayPercentComplete
         complete={achieved.linkedinNum}
         total={currentUser.linkedinGoal}
       />
+      <ButtonGroup variant="contained">
+        <Button onClick={() => handleButtonClick('linkedinNum', -1)}>-</Button>
+        <Button onClick={() => handleButtonClick('linkedinNum', 1)}>+</Button>
+      </ButtonGroup>
       <Typography component="h3">Coding Hours</Typography>
       <DisplayPercentComplete
         complete={achieved.codeNum}
         total={currentUser.codeGoal}
       />
+      <ButtonGroup variant="contained">
+        <Button onClick={() => handleButtonClick('codeNum', -1)}>-</Button>
+        <Button onClick={() => handleButtonClick('codeNum', 1)}>+</Button>
+      </ButtonGroup>
     </Container>
   );
 }
