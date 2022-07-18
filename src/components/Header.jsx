8f3181b5b2c858b67b5c9e84c76d77112fc50672
aidanliddiard <aidanliddiard@gmail.com>
2022-07-18 15:12:52 -0700

    added theme colors and fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { CssBaseline, Stack } from '@mui/material';
+import { CssBaseline, Stack, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
@@ -17,6 +17,7 @@ function Header() {
     <Box sx={{ height: '80px', borderBottom: '1px black solid' }}>
       <CssBaseline />
       <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Typography variant="h1">find(job)</Typography>
         {currentUser.email && (
           <CustomButton onClick={handleClick} type="submit" variant="contained">
             Logout
