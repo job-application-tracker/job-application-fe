efadfedaab59e3acdb1be71150d08d99bb045d9e
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-18 12:45:06 -0700

    merged

--- src/views/Progress.jsx
+++ src/views/Progress.jsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import ModalForm from '../components/ModalForm';
-=======
-import { Container, Grid } from '@mui/material';
-import React, { useEffect } from 'react';
->>>>>>> 9f340d1a15835153229fb609a6949104d434e67a
 import Sidebar from '../components/Sidebar';
 import { useUserContext } from '../context/userContext';
 import Board from './Board/Board';
@@ -22,11 +17,11 @@ export default function Progress() {
   }, []);
 
   return (
-      <>
-        <Button onClick={() => setOpen(true)} handleClose={() => setOpen(false)}>
-          Open
-        </Button>
-        <ModalForm open={open} />
+    <>
+      <Button onClick={() => setOpen(true)} handleClose={() => setOpen(false)}>
+        Open
+      </Button>
+      <ModalForm open={open} />
       <Grid container spacing={2}>
         <Grid item xs={2}>
           <Sidebar />
@@ -35,6 +30,6 @@ export default function Progress() {
           <Board />
         </Grid>
       </Grid>
-      </>
+    </>
   );
 }
